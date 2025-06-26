6c67f9abc5d077ad733da311814731400bb6067c
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-06-26 23:07:50 +0600

    post, get , dashbord done

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
         //mongo db er coder copy,paste
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.taikvqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,7 +24,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
+    const db = client.db('parcelService');
+    const parcelCollection = db.collection('parcels');
+    app.get('/parcels', async (req, res) => {
+      try {
+        const { email } = req.query;
+        const filter = {};
+        // If email is provided, filter by created_by field
+        if (email) {
+          filter.created_by = email;
+        }
+        const parcels = await parcelCollection
+          .find(filter)
+          .sort({ createdAt: -1 }) // Newest first
+          .toArray();
+
+        res.status(200).json(parcels);
+      } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+
+                // POST route to add parcel
+   app.post('/parcels', async (req, res) => {
+      try {
+        const parcelData = req.body;
+        console.log(parcelData)
+        const result = await parcelCollection.insertOne(parcelData);
+        res.status(201).send(result);
+      } catch (error) {
+        console.error('Error saving parcel:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -32,7 +65,7 @@ async function run() {
     // await client.close();
   }
 }
-
+run().catch(console.dir);
 app.get("/", (req,res)=>{
         // server e show kore
     res.send("zap shift server is running")
